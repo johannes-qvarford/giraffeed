299c38dcff471f3ed88a1e6a8b789944c644fdb5
Johannes Qvarford <jq.email+github@pm.me>
2023-02-05 21:07:41 +0100

    Reset input field after copying to clipboard.

--- src/main/resources/META-INF/resources/proxy.js
+++ src/main/resources/META-INF/resources/proxy.js
@@ -5,5 +5,7 @@ paste.addEventListener('click', async () => {
     const response = await fetch(`/proxied-url?source-url=${url.value}`)
     const proxyUrl = (await response.json()).proxy_url
     result.value = proxyUrl
+    // Make it easier to paste in another url later
+    url.value = ''
     navigator.clipboard.writeText(proxyUrl)
 });
