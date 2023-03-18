7290e3a19f587f62b4a760d70ec06ba7f28a6653
Johannes Qvarford <jq.email+github@pm.me>
2023-03-18 20:50:21 +0100

    Rename ProxyResource to HomeResource.

--- src/main/kotlin/net/qvarford/giraffeed/resources/HomeResource.kt
+++ src/main/kotlin/net/qvarford/giraffeed/resources/HomeResource.kt
@@ -8,7 +8,7 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
 @Path("/")
-class ProxyResource {
+class HomeResource {
     @CheckedTemplate
     object Templates {
         @JvmStatic
